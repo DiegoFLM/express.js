d4f9227aaa426d3cd3ee4c342e9f6d3fd5d24396
DiegoF <diego.ledesma@correounivalle.edu.co>
2022-10-11 13:05:45 -0500

    services added

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,70 +1,48 @@
 const express = require('express');
-//https://www.npmjs.com/package/@faker-js/faker
-const { faker } = require ('@faker-js/faker');
+
+const ProductService = require('../services/product.service');
 
 const router = express.Router();
+const service = new ProductService();
+
 
 router.get('/', (req, res) => {
-
-  const products = [];
-  const {size} = req.query;
-  const limit = size || 10;
-
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl()
-    });
-  }
-
+  const products = service.find();
   res.json(products);
 });
 
 router.get('/filter', (req, res) => {
-  const { name } = req.query;
-  res.json('I am filtering products by name: ' + name);
+  res.json('I am filtering products');
 });
 
 router.get('/:id', (req, res) => {
   // const id = req.params.id;
-
   //with ES6:
   const { id } = req.params;
-  res.json({
-    id,
-    name: 'iPhone',
-    price: 1000
-  });
+  const product = service.findOne(id);
+  res.json(product);
 });
 
 
 router.post('/', (req, res) => {
   const body = req.body;
-  res.json({
-    message: 'I am creating a product',
-    data: body
-  });
+  const newProduct = service.create(body);
+  res.status(201).json({newProduct});
 });
 
 
 router.patch('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
-  res.json({
-    message: 'partial update',
-    data: body,
-    id
-  });
+  const product = service.update(id, body);
+  res.json(product);
 });
 
 
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
-    message: 'deleted',
-    id
-  });
+  const ans = service.delete(id);
+  res.json({ans});
 });
 
 
