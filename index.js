d7f7f655fedac52e14cbdcb803fddfca2489bf96
DiegoF <diego.ledesma@correounivalle.edu.co>
2022-10-16 15:01:55 -0500

    deploy with heroku

--- index.js
+++ index.js
@@ -5,28 +5,27 @@ const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //express.json is a native middleware
 app.use(express.json());
 
 
 // include localhost in whitelist
-// const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'http://127.0.0.1:8080', 'http://127.0.0.1:5500',
-// '127.0.0.1:5500', 'http://localhost:80'];
+const whitelist = ['http://localhost:8080', 'http://platzi-store.co'];
 
-// const options = {
-//   origin: (origin, callback) => {             //
-//     if (whitelist.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       console.error('Error origin port: ', origin);
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   }
-// };
-// app.use(cors(options));
-app.use(cors());
+const options = {
+  origin: (origin, callback) => {             //
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      console.error('Error origin port: ', origin);
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+app.use(cors(options));
+// app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hi, I am an express server!');
