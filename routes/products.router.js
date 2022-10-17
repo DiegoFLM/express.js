00b838b04fb7495fa37ee6934a0649e9515e9985
DiegoF <diego.ledesma@correounivalle.edu.co>
2022-10-17 11:30:04 -0500

    setup of my_store

--- routes/products.router.js
+++ routes/products.router.js
@@ -8,10 +8,15 @@ const router = express.Router();
 const service = new ProductService();
 
 
-router.get('/', async (req, res) => {
-  const products = await service.find();
-  res.json(products);
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await service.find();
+    res.json(products);
+  } catch (error) {
+    next(error);
+  }
 });
+
 
 router.get('/filter', (req, res) => {
   res.json('I am filtering products');
@@ -33,12 +38,16 @@ router.get('/:id',
 
 router.post('/',
   validatorHandler(createProductSchema, 'body'),
-
-  async (req, res) => {
-    const body = req.body;
-    const newProduct = await service.create(body);
-    res.status(201).json({newProduct});
-});
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newProduct = await service.create(body);
+      res.status(201).json(newProduct);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 
 router.patch('/:id',
@@ -57,11 +66,17 @@ router.patch('/:id',
 });
 
 
-router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
-  const ans = await service.delete(id);
-  res.json({ans});
-});
-
+router.delete('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.status(201).json({id});
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
