56dec6d5dcf9ca7308841718096c2224d7649431
DiegoF <diego.ledesma@correounivalle.edu.co>
2022-11-04 16:44:33 -0500

    simultaneous creation of user and customer

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const productsRouter = require('./products.router');
 const categoriesRouter = require('./categories.router');
 const usersRouter = require('./users.router');
 const ordersRouter = require('./orders.router');
+const customerRouter = require('./customer.router');
 
 function routerApi(app) {
   const router = express.Router();
@@ -13,6 +14,7 @@ function routerApi(app) {
   router.use('/categories', categoriesRouter);
   router.use('/users', usersRouter);
   router.use('/orders', ordersRouter);
+  router.use('/customer', customerRouter);
 }
 
 module.exports = routerApi;
--- services/user.service.js
+++ services/user.service.js
@@ -13,7 +13,9 @@ class UserService {
   }
 
   async find() {
-    const response = await models.User.findAll();
+    const response = await models.User.findAll({
+      include: ['customer'],
+    });
     return response;
   }
 
